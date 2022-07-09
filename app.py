746bb57f25f7001ee7cc35ecf1d5eaa76ec77c59
Pooja Sreenivasan <104851237+pjo7@users.noreply.github.com>
2022-07-09 23:15:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model = joblib.load('Weather Prediction')
+model = joblib.load('Weather Prediction(1)')
 st.title('WEATHER PREDICTOR')
 ip = st.text_input('Enter a Year',value=0)
 op = model.predict([[ip]])
