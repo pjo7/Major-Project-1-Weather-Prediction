292dcb15c1f89377c93f5752c83cf2356e5d3fb8
Pooja Sreenivasan <104851237+pjo7@users.noreply.github.com>
2022-07-09 23:09:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 model = joblib.load('Weather Prediction')
 st.title('WEATHER PREDICTOR')
-ip = st.text_input('Enter a Year',value="0")
+ip = st.text_input('Enter a Year',value=0)
 op = model.predict([[ip]])
 if st.button('Predict'):
   st.title(op[0])
